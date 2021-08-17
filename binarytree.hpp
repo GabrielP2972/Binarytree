d8697017429980a6e2fbc859dd37b92a61583734
GabrielP2972 <86384342+GabrielP2972@users.noreply.github.com>
2021-08-17 10:50:08 -0400

    Add files via upload

---
+++ binarytree.hpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include"node_prev.h"
+using namespace std;
+
+template<class T>
+class 
---
+++ node.h
@@ -0,0 +1,16 @@
+#ifndef _node_h_
+#define _node_h_
+
+template <class T>
+class node {
+    public:
+	    T info;
+	    node <T> *next;
+
+    node(T info) {
+		this->info = info;
+		next = NULL;
+	}
+};
+
+#endif
